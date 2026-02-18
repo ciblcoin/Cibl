6997990fc2b68a1a7c698e666868a072c1640a08
CIB <ciblcoin@gmail.com>
2026-02-18 20:37:52 +0330

    Create src/utils/AssetMap.js

---
+++ src/utils/AssetMap.js
@@ -0,0 +1,17 @@
+export const NetworkIcons = {
+  BTC: require('../../assets/icons/networks/btc.svg'),
+  ETH: require('../../assets/icons/networks/eth.svg'),
+  SOL: require('../../assets/icons/networks/sol.svg'),
+  TON: require('../../assets/icons/networks/ton.svg'),
+  SUI: require('../../assets/icons/networks/sui.svg'),
+  BASE: require('../../assets/icons/networks/base.svg'),
+};
+
+export const NetworkColors = {
+  BTC: '#F7931A',
+  ETH: '#627EEA',
+  SOL: '#14F195',
+  TON: '#0088CC',
+  SUI: '#6FBED5',
+  BASE: '#0052FF',
+};
