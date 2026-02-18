284dcf4dc17a9c9bb32d1813376dd3cd2bc46433
CIB <ciblcoin@gmail.com>
2026-02-19 01:51:23 +0330

    Update InteractionService.js

--- src/utils/InteractionService.js
+++ src/utils/InteractionService.js
@@ -3,21 +3,22 @@ import * as Haptics from 'expo-haptics';
 
 export const InteractionService = {
   playInteraction: async (theme) => {
-    // ۱. اجرای لرزش (Haptic)
-    Haptics.impactAsync(theme.hapticType);
+    // Execute Haptic
+    if (theme.hapticType) {
+      Haptics.impactAsync(theme.hapticType);
+    }
 
-    // ۲. پخش صدا (Sound)
+    // Play Sound
     try {
       const { sound } = await Audio.Sound.createAsync(theme.sound);
       await sound.playAsync();
-      // آزادسازی حافظه بعد از پخش
       sound.setOnPlaybackStatusUpdate(async (status) => {
         if (status.didJustFinish) {
           await sound.unloadAsync();
         }
       });
     } catch (error) {
-      console.log("Sound play error", error);
+      console.log("Audio Feedback Error:", error);
     }
   }
 };
