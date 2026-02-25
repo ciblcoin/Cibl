ce0eac2f89e5e756662cce950457047926d512b8
CIB <ciblcoin@gmail.com>
2026-02-25 18:26:27 +0330

    Delete src/blockchain/btc directory

--- src/blockchain/btc/config.js
+++
@@ -1,8 +0,0 @@
-export const BTC_CONFIG = {
-  NETWORK: 'mainnet', // یا 'testnet' برای تست
-  SYMBOL: 'BTC',
-  COLOR: '#F7931A', // نارنجی کلاسیک بیت‌کوین
-  // استفاده از سرویس‌هایی مثل Blockstream یا Blockchain.info برای دریافت دیتای شبکه
-  API_URL: 'https://blockstream.info/api',
-  COINGECKO_ID: 'bitcoin'
-};
